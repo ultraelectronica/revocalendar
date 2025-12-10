2fe1e87ccf39a49e2fdc54878327fe11705c7018
ultraelectronica <fyketonel@gmail.com>
2025-12-10 14:41:35 +0800

    Removed a comment

--- middleware.ts
+++ middleware.ts
@@ -29,14 +29,6 @@ export async function middleware(request: NextRequest) {
 // Configure which routes the middleware runs on
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - public folder files
-     * Feel free to modify this pattern to include more paths.
-     */
     '/((?!_next/static|_next/image|favicon.ico|favicon.svg|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 };
